7f6f457f4cd301a53e380228165fe434732c2827
alicnngree <155579627+alicnngree@users.noreply.github.com>
2025-01-16 14:08:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json());
 
 // MySQL bağlantısı
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'arac_yukleme'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
